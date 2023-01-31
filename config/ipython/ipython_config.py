aa3a81575d513bd6aa90eb1658d42a47b37d3669
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2023-01-30 23:16:58 -0500

    refactor: modified ipython config and pyproject settings

--- config/ipython/ipython_config.py
+++ config/ipython/ipython_config.py
@@ -1,25 +1,22 @@
 # source: https://ipython.readthedocs.io/en/stable/config/intro.html#example-configuration-file
-# source: https://ipython.org/ipython-doc/3/config/intro.html 
-# c = get_config()
+# source: https://ipython.org/ipython-doc/3/config/intro.html
+c = get_config()
 
 c.TerminalIPythonApp.display_banner = True
 c.InteractiveShellApp.log_level = 20
 # c.InteractiveShellApp.extensions = [
 #     'myextension'
 # ]
-c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    'import pandas as pd'
-]
+c.InteractiveShellApp.exec_lines = ["import numpy as np", "import pandas as pd"]
 # c.InteractiveShellApp.exec_files = [
 #     'mycode.py',
 #     'fancy.ipy'
 # ]
 c.InteractiveShell.autoindent = True
-c.InteractiveShell.colors = 'linux'
+c.InteractiveShell.colors = "linux"
 c.InteractiveShell.confirm_exit = False
-c.InteractiveShell.editor = 'nvim'
-c.InteractiveShell.xmode = 'Context'
+c.InteractiveShell.editor = "nvim"
+c.InteractiveShell.xmode = "Context"
 
 # c.PromptManager.in_template  = 'In [\#]: '
 # c.PromptManager.in2_template = '   .\D.: '
@@ -28,7 +25,4 @@ c.InteractiveShell.xmode = 'Context'
 
 c.PrefilterManager.multi_line_specials = True
 
-c.AliasManager.user_aliases = [
- ('la', 'ls -al')
-]
-
+c.AliasManager.user_aliases = [("la", "ls -al")]
